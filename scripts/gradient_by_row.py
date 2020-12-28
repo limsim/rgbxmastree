d5360c73a6af81698a10f766120c21ef44eae185
Lim Sim <lim.sim@gmail.com>
2020-12-28 21:34:22 +0000

    tidy up code

--- scripts/gradient_by_row.py
+++ scripts/gradient_by_row.py
@@ -7,13 +7,7 @@ from decimal import *
 getcontext().prec = 1
 
 tree = RGBXmasTree()
-# tree.brightness = 0.04 
-
-def random_colour():
-  r = Decimal(random.random())
-  g = Decimal(random.random())
-  b = Decimal(random.random())
-  return (r, g, b)
+tree.brightness = 0.5
 
 def generate_random_gradient():
   colourGradient = []
@@ -45,7 +39,8 @@ def generate_random_gradient():
     ['blue', 'red'],
     ['red', 'green'],
     ['green', 'red'],
-    ['blue', 'magenta']
+    ['blue', 'magenta'],
+    ['#f77f00', '#55a630']
   ]
 
   # Workout the number of colour pairs available.
@@ -62,17 +57,6 @@ def generate_random_gradient():
 
   return colourGradient
 
-
-column1 = [0,1,2,3] #1
-column2 = [4,5,6] #4
-column3 = [7,8,9] #8
-column4 = [10,11,12] #5
-column5 = [13,14,15] #7
-column6 = [16,17,18] #6
-column7 = [19,20,21] #3
-column8 = [22,23,24] #2
-theColumnTree = [column1, column2, column3, column4, column5, column6, column7, column8]
-
 # The sequence of pixels to light up so that they appear to go in rows
 rowOrder = [0,24,19,6,12,16,15,7,1,23,20,5,11,17,14,8,2,22,21,8,10,18,13,9]
 # This order includes the pixel at the top of the tree.
@@ -87,6 +71,7 @@ try:
     # Turn the pixels on pixel by pixel with each colour on the generate_random_gradient.
     for x in range(24):
       tree[rowOrder[x]].color = randomColourGradient[x]
+      sleep(0.05)
       if x == (numberOfPixels - 1):
         sleep(0.5)       
     
